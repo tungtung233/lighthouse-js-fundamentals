89fb170e1148a0ab1983c0f47ee1c200969802ea
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-05-07 19:13:22 +0000

    Calculate anyone's age

---
+++ ageCalculator.js
@@ -0,0 +1,6 @@
+function ageCalculator (name, yearOfBirth, currentYear) {
+  let age = currentYear - yearOfBirth;
+  return (`${name} is ${age} years old.`);
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
