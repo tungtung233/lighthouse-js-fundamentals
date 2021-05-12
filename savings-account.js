5775571e2eeffd2367f069d6e22ea6c6f4c8a6da
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-05-12 21:06:35 +0000

    savings account - verify balance, deposit, withdraw

---
+++ savings-account.js
@@ -0,0 +1,18 @@
+var savingsAccount = {
+  balance: 1000,
+  interestRatePercent: 1,
+  deposit: function addMoney(amount) {
+      if (amount > 0) {
+          savingsAccount.balance += amount;
+      }
+  },
+  withdraw: function removeMoney(amount) {
+      var verifyBalance = savingsAccount.balance - amount;
+      if (amount > 0 && verifyBalance >= 0) {
+          savingsAccount.balance -= amount;
+      }
+  },
+
+};
+
+console.log(savingsAccount.withdraw(25));
