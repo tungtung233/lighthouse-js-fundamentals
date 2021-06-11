bafdf4db3fbd0dc5e9f2d076ba8b02986c511e6e
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-11 18:08:05 +0000

    list of things to pack

--- packing-list.js
+++ packing-list.js
@@ -5,5 +5,8 @@ console.log("Kitchen stuff to pack:");
 let i = 0;
 while (i < packingList.length) {
   console.log(packingList[i]);
+<<<<<<< HEAD
   i++;
+=======
+>>>>>>> parent of da0847b... List of everything to pack
 }
