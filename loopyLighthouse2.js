c0a1d279576769e93328b8587b3c0395130bb00b
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-21 22:12:25 +0000

    refactored the code

--- loopyLighthouse2.js
+++ loopyLighthouse2.js
@@ -1,4 +1,4 @@
-function loopyLighthouse(range, multiples, words) {
+let loopyLighthouse = function(range, multiples, words) {
   for (let x = range[0]; x <= range[1]; x++) {
     if (x % multiples[0] === 0 && x % multiples[1] === 0) {
       console.log(words[0] + words[1]);
@@ -7,10 +7,10 @@ function loopyLighthouse(range, multiples, words) {
     } else if (x % multiples[1] === 0) {
       console.log(words[1]);
     } else {
-      console.log(x)
+      console.log(x);
     }
   }
-}
+};
 
 
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
